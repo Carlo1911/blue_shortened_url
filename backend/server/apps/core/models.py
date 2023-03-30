0ed52fcde6a851d23992700afad2ab75af06a849
Carlo1911 <carlo.alva@pucp.pe>
2023-03-30 17:17:28 -0500

    Define Shortened Url model

---
+++ backend/server/apps/core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class ShortenedUrl(models.Model):
+    slug = models.SlugField("Original URL", max_length=100, unique=True)
+    shortened_slug = models.CharField("Shortened URL", max_length=100, unique=True)
+    title = models.CharField("Title", max_length=100, blank=True)
+    total_visits = models.IntegerField("Total visits", default=0)
+    created_at = models.DateTimeField("Created at", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated at", auto_now=True)
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        return self.shortened_slug
+
+    class Meta:
+        verbose_name = "Shortened URL"
+        verbose_name_plural = "Shortened URLs"
